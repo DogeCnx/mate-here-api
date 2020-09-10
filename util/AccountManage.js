379db7e97aa12900514f3e87a338dc502c9a3c5a
DogeCnx <Saratoga1412@gmail.com>
2020-09-11 05:31:02 +0700

    Update AccountManage.js

--- util/AccountManage.js
+++ util/AccountManage.js
@@ -1,6 +1,7 @@
 
 class AccountManage {
-    constructor(AccountModel) {
+    
+    constructor (AccountModel) {
       this._Account = AccountModel
     }
   
@@ -19,38 +20,53 @@ class AccountManage {
         
         return this._withReferences(accounts,references).fetch().then(response => response.first())
     }
-    getById(account_id,references){
-        const accounts = this._Account.query().where('account_id',account_id)
+    getById(accountInstance,references){
+        const { id } = accountInstance.params
+        const accounts = this._Account.query().where({account_id : id})
         
         
         return this._withReferences(accounts,references).fetch().then(response => response.first())
     }
 
     async create(accountInstance,references) {
-        const {account_id} = this._Account.create(accountInstance)
-        const accounts = this._Account.query().where('account_id',account_id)
+
+        const {account_id} =  await this._Account.create(accountInstance.body)
+
+        const accounts = this._Account.query().where({account_id : account_id})
 
         return this._withReferences(accounts,references).fetch().then(response => response.first())
     }
     
-    async deletById(account_id){
-        const accounts = await this._Account.find(account_id)
+    async deletById(accountInstance){
+        const { id } = accountInstance.params
+        const accounts = await this._Account.find(id)
+
+        if(!accounts){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
         accounts.delete()
         await accounts.save();
 
-
-        return this._withReferences(accounts,references).fetch().then(response => response.first())
+        return {status : 200 ,error : undefined , data : 'complete'};
     }
 
-    async updateById(account_id,accountInstance,references){
-        const accounts = await this._Account.find(account_id)
-        accounts.merge(accountInstance)
+    async updateById(accountInstance,references){
+        const { id } = accountInstance.params
+        let accounts = await this._Account.find(id)
+
+        if(!accounts){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+
+        accounts.merge(accountInstance.body)
         await accounts.save();
     
-        const accounts = this._Account.query().where('account_id',account_id)
+        accounts = this._Account.query().where({account_id : id})
         
         return this._withReferences(accounts,references).fetch().then(response => response.first())
     }
 
 
 }
+
+module.exports = AccountManage
