9ba13c226d30a5a59572537a29bd5d2df9263d1f
DogeCnx <Saratoga1412@gmail.com>
2020-09-10 21:55:27 +0700

    Add util and Update AccountManage.js

---
+++ util/AccountManage.js
@@ -0,0 +1,56 @@
+
+class AccountManage {
+    constructor(AccountModel) {
+      this._Account = AccountModel
+    }
+  
+    _withReferences(instance, references) {
+      if (references) {
+        const extractedReferences = references.split(",")
+        instance.with(extractedReferences)
+      }
+  
+      return instance;
+    }
+
+    getAll(references){
+        const accounts = this._Account.query()
+        
+        
+        return this._withReferences(accounts,references).fetch().then(response => response.first())
+    }
+    getById(account_id,references){
+        const accounts = this._Account.query().where('account_id',account_id)
+        
+        
+        return this._withReferences(accounts,references).fetch().then(response => response.first())
+    }
+
+    async create(accountInstance,references) {
+        const {account_id} = this._Account.create(accountInstance)
+        const accounts = this._Account.query().where('account_id',account_id)
+
+        return this._withReferences(accounts,references).fetch().then(response => response.first())
+    }
+    
+    async deletById(account_id){
+        const accounts = await this._Account.find(account_id)
+        accounts.delete()
+        await accounts.save();
+
+
+        return this._withReferences(accounts,references).fetch().then(response => response.first())
+    }
+
+    async updateById(account_id){
+        const accounts = await this._Account.find(account_id)
+        accounts.merge()
+        await accounts.save();
+    
+        const accounts = this._Account.query().where('account_id',account_id)
+        
+        return this._withReferences(accounts,references).fetch().then(response => response.first())
+    }
+
+
+}
