f99861dabc75d668ceb8a174b7d2d6a5978f803d
Cruzel28 <gin_ko28@hotmail.com>
2020-09-11 17:44:15 +0700

    Update CentralController.js

--- app/Controllers/Http/CentralController.js
+++ app/Controllers/Http/CentralController.js
@@ -1,32 +1,72 @@
 'use strict'
+const Central = use('App/Models/Central')
+const CentralManage = require('../../../util/CentralManage')
 
 class CentralController {
     async index( {request }) {
 
-
+        const {references = undefined} =request.qs
+        const centralManage = new CentralManage(Central)
+        const centrals = await centralManage.getAll(references)
+        
+        
+        return {status : 200 ,
+               error : undefined , 
+               data : centrals};
        
     }
       
 
     async show({ request }) {
-
+    
+        const {references = undefined} =request.qs
+        const centralManage = new CentralManage(Central)
+        const centrals = await centralManage
+        .getById(request ,references)
+        
+        return {status : 200 ,
+               error : undefined , 
+               data : centrals};
     }
 
 
     async store({ request }) {
 
-  
+        const {references = undefined} =request.qs
+        const centralManage = new CentralManage(Central)
+        const centrals = await centralManage
+        .create(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : centrals}; 
     }
 
 
     async update( {request} ) {
-  
+        
+        const {references = undefined} =request.qs
+        const centralManage = new VCentralManage(Central)
+        const centrals = await centralManage
+        .updateById(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : centrals};
 
     }
 
   
     async destroy  ({ request }){
 
+        const {references = undefined} =request.qs
+        const centralManage = new CentralManage(Central)
+        const centrals = await centralManage
+        .deletById(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : centrals};
 
     }
 }
