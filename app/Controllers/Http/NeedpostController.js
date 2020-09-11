dc53eb0aaa953503230d2025a0e1587a82ca0caa
Cruzel28 <gin_ko28@hotmail.com>
2020-09-11 20:01:25 +0700

    Update NeedpostController.js

--- app/Controllers/Http/NeedpostController.js
+++ app/Controllers/Http/NeedpostController.js
@@ -1,32 +1,69 @@
 'use strict'
+const Needpost = use('App/Models/Needpost')
+const NeedpostManage = require('../../../util/Needpost')
+
 
 class NeedpostController {
+
     async index( {request }) {
-
-
-       
+        const {references = undefined} =request.qs
+        const needpostManage = new NeedpostManage(Needpost)
+        const needposts = await needpostManage
+        .getAll(references)
+        
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : needposts};
     }
       
 
     async show({ request }) {
-
+        const {references = undefined} =request.qs
+        const needpostManage = new NeedpostManage(Needpost)
+        const needposts = await needpostManage
+        .getById(request ,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : needposts};
     }
 
 
     async store({ request }) {
-
-  
+        const {references = undefined} =request.qs
+        const needpostManage = new NeedpostManage(Needpost)
+        const needposts = await needpostManage
+        .create(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : needposts};
     }
 
 
     async update( {request} ) {
-  
+        const {references = undefined} =request.qs
+        const needpostManage = new NeedpostManage(Needpost)
+        const needposts = await needpostManage
+        .updateById(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : needposts};
 
     }
 
   
     async destroy  ({ request }){
-
+        const {references = undefined} =request.qs
+        const needpostManage = new NeedpostManage(Needpost)
+        const needposts = await needpostManage
+        .deletById(request,references)
+        
+        return {status : 200 ,
+            error : undefined , 
+            data : needposts};
 
     }
 }
