0c4e70d3ecb17e71dc83c1d34c50a810225cf1a9
DogeCnx <Saratoga1412@gmail.com>
2020-09-13 20:16:25 +0700

    Add RegisterController

---
+++ app/Controllers/Http/RegisterController.js
@@ -0,0 +1,56 @@
+'use strict'
+const Client = use('App/Models/Client')
+const Account = use('App/Models/Account')
+const RegisterUtil = require('../../../util/RegisterUtil')
+
+class RegisterController {
+    async index( {request }) {
+        const {references = undefined} =request.qs
+        const registerUtil = new RegisterUtil(Account)
+        const registers = await registerUtil.getAll(references)
+        
+        
+        return {status : 200 ,error : undefined , data : registers};
+    }
+      
+
+    async show({ request }) {
+        const {references = undefined} =request.qs
+        const registerUtil = new RegisterUtil(Account)
+        const registers = await registerUtil.getById(request,references)
+        
+        return {status : 200 ,error : undefined , data : registers};
+    }
+
+
+    async store({ request }) {
+
+        const {references = undefined} =request.qs
+        const registerUtil = new RegisterUtil(Account)
+        const registers = await registerUtil.create(request,references)
+        
+        return {status : 200 ,error : undefined , data : registers};
+
+    }
+
+
+    async update( {request} ) {
+        const {references = undefined} =request.qs
+        const registerUtil= new RegisterUtil(Account)
+        const registers= await registerUtil.updateById(request,references)
+        
+        return {status : 200 ,error : undefined , data :registers};
+
+    }
+
+    async destroy  ({ request }){
+        const {references = undefined} =request.qs
+        const  registerUtil  = new  RegisterUtil (Account)
+        const  registers = await  registerUtil.deletById(request,references)
+        
+        return {status : 200 ,error : undefined , data : registers };
+
+    }
+}
+
+module.exports = RegisterController
