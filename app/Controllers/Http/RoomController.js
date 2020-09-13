86f251f88b734195a7ac1614736965d79ddc4ddd
Cruzel28 <gin_ko28@hotmail.com>
2020-09-13 11:31:13 +0700

    Update RoomController.js call validator

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -1,6 +1,9 @@
 'use strict'
 const Room  = use('App/Models/Room')
 const RoomManage = require('../../../util/Room')
+const Validator = use('Validator')
+const roomValidator = require('../../../service/RoomValidator')
+
 
 
 class RoomController {
@@ -23,7 +26,14 @@ class RoomController {
         const roomManage = new RoomManage(Room)
         const rooms = await roomManage
         .getById(request ,references)
-        
+
+        const validatedValue = numberTypeParamValidator(id)
+
+       if(validatedValue.error) 
+       return { status: 500, 
+        error: validatedValue.error, 
+        data: undefined}
+
         return {status : 200 ,
             error : undefined , 
             data : rooms};
@@ -33,6 +43,14 @@ class RoomController {
     async store({ request }) {
         const {references = undefined} =request.qs
         const roomManage = new RoomManage(Room)
+        const validation = await roomValidator(request.body)
+      
+      if(validation.error){
+        return {status: 422, 
+          error: validation.error,
+          data: undefined}
+      }
+
         const rooms = await roomManage
         .create(request,references)
         
@@ -45,6 +63,14 @@ class RoomController {
     async update( {request} ) {
         const {references = undefined} =request.qs
         const roomManage = new RoomManage(Room)
+        const validation = await roomValidator(request.body)
+      
+      if(validation.error){
+        return {status: 422, 
+          error: validation.error,
+          data: undefined}
+      }
+
         const rooms = await roomManage
         .updateById(request,references)
         
