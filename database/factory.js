532737677d5f969260acf44860f192e086363845
Cruzel28 <gin_ko28@hotmail.com>
2020-09-13 13:58:55 +0700

    Update factory.js Havepost

--- database/factory.js
+++ database/factory.js
@@ -26,13 +26,33 @@ Factory.blueprint('App/Models/Client' , (faker) => {
         first_name : faker.first() ,
         last_name : faker.last(),
         email : faker.email(),
-        telephone_number : faker.number({length:10}),
+        telephone_number : faker.integer({length:10}),
         line_id : faker.word({length:8}),
         facebook_name : faker.word(),
         date_of_birth : faker.date(),
         gender : faker.gender(),
         profile_picture : faker.avatar(),
-        account_id : faker.avatar(),
+        account_id : faker.avatar()
+    }
+})
+
+Factory.blueprint('App/Models/Havepost' , (faker) => {
+    return {
+        need_university_name: faker.word(),
+        type_room: faker.word(),
+        full_cost: faker.integer(),
+        half_cost: faker.integer(),
+        amount_of_mate: faker.integer(),
+        location: faker.sentence(),
+        faculty: faker.word(),
+        faculty_mate : faker.word(),
+        habit : faker.word(),
+        habit_mate : faker.word(),
+        like_thing : faker.word(),
+        dislike : faker.word(),
+        other_information : faker.paragraph(),
+        status_post : faker.word(),
+        client_id : faker.integer()
     }
 })
 
