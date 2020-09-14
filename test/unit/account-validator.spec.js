19f1fd073b487e4ad7d3fc8dac491c8954b5d948
Cruzel28 <gin_ko28@hotmail.com>
2020-09-14 23:21:35 +0700

    Update account-validator.spec.js

--- test/unit/account-validator.spec.js
+++ test/unit/account-validator.spec.js
@@ -3,19 +3,39 @@
 const { test } = use('Test/Suite')('Login Validator')
 const loginValidator = require('../../service/LoginValidator')
 
+test('should receive object as first parameter', 
+async ({ assert }) => {
+  
+  const validatedData = await loginValidator({
+    username: " ",
+    password: "asdfrtfw"
+  })
+  assert.isArray(validatedData.error)
+})
+
 test('should return error when pass incorrect data', 
 async ({ assert }) => {
   
   const validatedData = await loginValidator({
-    username: "JohnDoe123",
-    password: "asdfrtfw"
+    username: " ",
+    password: " "
   })
-  assert.isOk(validatedData)
-
-  const validatedData2 = await loginValidator(
-    "JohnDoe123", 
-    "1234"
-    )
-  assert.isNotOk(validatedData2)
+  assert.isArray(validatedData.error)
 })
 
+test('should return only one error if single incorrect data is passed', async ({ assert }) => {
+  const validatedData = await loginValidator({
+    username: "Johnlazy",
+    password: "Doe"
+  })
+  assert.equal(validatedData.error.length, 1)
+})
+
+test('should return undefined when pass correct data', async ({ assert }) => {
+  const validatedData = await loginValidator({
+    username: "asddasdadd",
+    password: "467854545"
+  })
+
+  assert.equal(validatedData.error, undefined)
+})
