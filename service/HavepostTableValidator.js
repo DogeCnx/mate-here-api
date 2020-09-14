d6b1fab52932e32f6c135f491e9ceb6bbb4955bd
Cruzel28 <gin_ko28@hotmail.com>
2020-09-14 22:22:50 +0700

    Update HavepostTableValidator.js

--- service/HavepostTableValidator.js
+++ service/HavepostTableValidator.js
@@ -10,7 +10,12 @@ module.exports =  async function HavepostTableValidator(data) {
            amount_of_mate,location,faculty,
            faculty_mate,habit,habit_mate,
            routine,like_thing,dislike,other_information,
-           status_post,client_id} = data
+           status_post,client_id,parking,lift,
+           keycard,security,
+           pool,gym,luandry,air_conditioner,number_of_toilet,
+           number_of_bedroom,pets,smoking,
+           internet_wifi,furniture,water_heater
+       } = data
     
     const rules = {
         username :'required|min:6|max:15' ,
@@ -28,7 +33,24 @@ module.exports =  async function HavepostTableValidator(data) {
         dislike : 'required|max:150',
         other_information : 'required|max:250',
         status_post : 'required|max:10',
-        client_id : 'required'
+        client_id : 'required',
+        username :'required|min:6|max:15' ,
+        parking : 'required|max:5',
+        lift : 'required|max:5',
+        keycard : 'required|max:5',
+        security : 'required|max:5',
+        pool : 'required|max:5',
+        gym : 'required|max:5',
+        luandry : 'required|max:5',
+        username :'required|min:6|max:15' ,
+        air_conditioner : 'required|max:5',        
+        number_of_toilet : 'required|max:5',        
+        number_of_bedroom : 'required|max:5',        
+        pets : 'required|max:5',        
+        smoking : 'required|max:5',        
+        internet_wifi : 'required|max:5',        
+        furniture : 'required|max:5',        
+        water_heater : 'required|max:5'  
         
     }
     
@@ -38,7 +60,12 @@ module.exports =  async function HavepostTableValidator(data) {
         amount_of_mate,location,faculty,
         faculty_mate,habit,habit_mate,
         routine,like_thing,dislike,other_information,
-        status_post,client_id},rules)
+        status_post,client_id,username,parking,lift,
+        keycard,security,
+        pool,gym,luandry,air_conditioner,number_of_toilet,
+        number_of_bedroom,pets,smoking,
+        internet_wifi,furniture,water_heater
+    },rules)
 
     return {
         error : validation.messages()
