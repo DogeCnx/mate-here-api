de3758cff28b163053680a2757e1ba979d5ff76e
Cruzel28 <gin_ko28@hotmail.com>
2020-09-14 22:27:14 +0700

    Update NeedpostTableValidator.js

--- service/NeedpostTableValidator.js
+++ service/NeedpostTableValidator.js
@@ -11,7 +11,11 @@ module.exports =  async function NeedpostTableValidator(data) {
            need_faculty_mate,need_habit,need_habit_mate,
            need_routine,need_like_thing,need_dislike,
            need_other_information,
-           status_post,client_id} = data
+           status_post,client_id,parking,lift,
+           keycard,security,
+           pool,gym,luandry,air_conditioner,number_of_toilet,
+           number_of_bedroom,pets,smoking,
+           internet_wifi,furniture,water_heater} = data
     
     const rules = {
 
@@ -30,7 +34,23 @@ module.exports =  async function NeedpostTableValidator(data) {
         need_dislike : 'required|max:150',
         need_other_information : 'required|max:250',
         status_post : 'required|max:10',
-        client_id : 'required'
+        client_id : 'required',
+        parking : 'required|max:5',
+        lift : 'required|max:5',
+        keycard : 'required|max:5',
+        security : 'required|max:5',
+        pool : 'required|max:5',
+        gym : 'required|max:5',
+        luandry : 'required|max:5',
+        air_conditioner : 'required|max:5',        
+        number_of_toilet : 'required|max:5',        
+        number_of_bedroom : 'required|max:5',        
+        pets : 'required|max:5',        
+        smoking : 'required|max:5',        
+        internet_wifi : 'required|max:5',        
+        furniture : 'required|max:5',        
+        water_heater : 'required|max:5' 
+
     }
     
     const validation = await Validator.validateAll({
@@ -40,7 +60,11 @@ module.exports =  async function NeedpostTableValidator(data) {
         need_faculty_mate,need_habit,need_habit_mate,
         need_routine,need_like_thing,need_dislike,
         need_other_information,
-        status_post,client_id},rules)
+        status_post,client_id,parking,lift,
+        keycard,security,
+        pool,gym,luandry,air_conditioner,number_of_toilet,
+        number_of_bedroom,pets,smoking,
+        internet_wifi,furniture,water_heater},rules)
 
     return {
         error : validation.messages()
